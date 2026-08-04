44ee4fafb36ce32535f99338fac2b9a296af5841
Sanjay <iknowsanjay@gmail.com>
2026-08-04 23:59:09 +0530

    feat: add a simple page (#8)

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'; 
-import { createRoot } from 'react-dom/client'; 
-import './index.css'; 
-import Portfolio from './Portfolio';
-
-createRoot(document.getElementById('root')).render(<Portfolio />);
-
